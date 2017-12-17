54ee473564c28b49561e7397dd60db027b4673bd
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2017-12-17 22:10:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.detect do |i|
-      @board[i[0]] == @board[i[1]] && @board[i[0]] == @board[i[2]] && current_player
+      @board[i[0]] == @board[i[1]] && @board[i[0]] == @board[i[2]]
     end
   end
 
